64701cbfbfa79e0d3c2f1e442f214578ac3737a7
techwebpixelcraft <techwebpixelcraft@gmail.com>
2025-11-26 19:21:30 +0530

    add vercel components

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./App.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
