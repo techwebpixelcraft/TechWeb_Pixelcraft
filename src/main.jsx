37dc59f99be6bb8513afd7c576045a22477d63cc
techwebpixelcraft <techwebpixelcraft@gmail.com>
2025-11-26 19:16:03 +0530

    vercel analytics install

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
